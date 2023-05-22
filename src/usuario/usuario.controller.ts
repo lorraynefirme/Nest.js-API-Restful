8a2d45190cc79ed11660492c967917497ad8c93a
Lorrayne Firme <44826803+lorraynefirme@users.noreply.github.com>
2023-05-22 19:55:37 -0300

    Add files via upload

---
+++ src/usuario/usuario.controller.ts
@@ -0,0 +1,62 @@
+import { Controller, Post, Get, Put, Delete, Body, Param } from "@nestjs/common";
+import { UsuarioRepository } from "./usuario.repository";
+import { CriaUsuarioDTO } from "./dto/CriaUsuario.dto";
+import { UsuarioEntity } from "./usuario.entity";
+import { v4 as uuid} from 'uuid'
+import { ListaUsuarioDTO } from "./dto/ListaUsuario.dto";
+import { AtualizaUsuarioDTO } from "./dto/AtualizaUsuario.dto";
+
+@Controller('/usuarios')
+export class UsuarioController{
+    constructor(private usuarioRepository: UsuarioRepository) {
+    }
+
+    @Post()
+    async criaUsuario(@Body() dadosDoUsuario: CriaUsuarioDTO){ 
+        const usuarioEntity = new UsuarioEntity();
+        usuarioEntity.email = dadosDoUsuario.email ;
+        usuarioEntity.senha = dadosDoUsuario.senha;
+        usuarioEntity.nome = dadosDoUsuario.nome;
+        usuarioEntity.id = uuid();
+
+        this.usuarioRepository.salvar(usuarioEntity)
+        return { 
+            //id: usuarioEntity.id, 
+            usuario: new ListaUsuarioDTO(usuarioEntity.id, usuarioEntity.nome),
+            message: 'Usuário criado com sucesso' 
+        }
+    }
+
+    @Get()
+    async listUsuarios(){
+        const usuariosSalvos = await this.usuarioRepository.listar();
+        const usuariosLista = usuariosSalvos.map(usuario => new ListaUsuarioDTO(
+                usuario.id,
+                usuario.nome,
+            )
+        );
+
+        return usuariosLista;
+    }
+
+    @Put('/:id')
+    async atualizaUsuario(@Param('id') id: string, @Body() novosDados: AtualizaUsuarioDTO){
+        const usuarioAtualizado = await this.usuarioRepository.atualiza(id, novosDados);
+
+        return {
+            usuario: usuarioAtualizado,
+            message: 'Usuário atualizado com sucesso'
+        }
+    }
+
+ 
+    @Delete('/:id')
+        async removeUsuario(@Param('id') id: string) {
+        const usuarioRemovido = await this.usuarioRepository.remove(id);
+
+        return {
+            usuario: usuarioRemovido,
+            mensagem: 'usuário removido com sucesso'
+        }
+    }
+}
---
+++ src/usuario/validacao/email-eh-unico.validator.ts
@@ -0,0 +1,28 @@
+import { ValidationArguments, ValidationOptions, ValidatorConstraint, ValidatorConstraintInterface, registerDecorator } from "class-validator";
+import { UsuarioRepository } from "../usuario.repository";
+import { Injectable } from "@nestjs/common";
+
+@Injectable() //transformando em provider
+@ValidatorConstraint({ async: true }) //transformando em validator assincrono
+export class EmailEhUnicoValidator implements ValidatorConstraintInterface {
+
+    constructor(private usuarioRepository: UsuarioRepository) {}
+
+    async validate(value: any, validationArguments?: ValidationArguments): Promise<boolean> {
+        const usuarioComEmailExiste = await this.usuarioRepository.existeComEmail(value);
+        return !usuarioComEmailExiste;
+    }
+}
+
+export const EmailEhUnico = (opcoesDeValidacao: ValidationOptions) => { 
+    return (objeto: Object, propriedade: string) => {
+        registerDecorator({
+            target: objeto.constructor,
+            propertyName: propriedade,
+            options: opcoesDeValidacao,
+            constraints: [],
+            validator: EmailEhUnicoValidator
+        });
+    }
+
+}
